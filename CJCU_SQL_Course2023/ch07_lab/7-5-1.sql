b91da0fe0e83874a63294e57d807976583c9c688
Jun-an <evan20021025@gmail.com>
2023-11-26 19:04:09 +0800

    Update 7-5-1.sql

--- CJCU_SQL_Course2023/ch07_lab/7-5-1.sql
+++ CJCU_SQL_Course2023/ch07_lab/7-5-1.sql
@@ -8,51 +8,51 @@ USE ch07_lab
 -- 1.
 
 CREATE TABLE Department (
-    Dep_code CHAR(5),
-	Dep_name NVARCHAR(10) NOT NULL,
-	Dep_chair NVARCHAR(10),
-	PRIMARY KEY(Dep_code)
+Dep_code CHAR(5),
+Dep_name NVARCHAR(10) NOT NULL,
+Dep_chair NVARCHAR(10),
+PRIMARY KEY(Dep_code)
 )
 
 -- 2.
 
 CREATE TABLE Teacher (
-    Teacher_id CHAR(5),
-	Teacher_name NVARCHAR(10) NOT NULL,
-	Dep_code CHAR(5),
-	PRIMARY KEY(Teacher_id),
-	FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE
+Teacher_id CHAR(5),
+Teacher_name NVARCHAR(10) NOT NULL,
+Dep_code CHAR(5),
+PRIMARY KEY(Teacher_id),
+FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE
 )
 
 CREATE TABLE Student (
-    Stu_id CHAR(5),
-	Stu_name NVARCHAR(10) NOT NULL,
-	gender NCHAR(1),
-	Dep_code CHAR(5),
-	PRIMARY KEY(Stu_id),
-	FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE
+Stu_id CHAR(5),
+Stu_name NVARCHAR(10) NOT NULL,
+gender NCHAR(1),
+Dep_code CHAR(5),
+PRIMARY KEY(Stu_id),
+FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE
 )
 
 -- 3.
 
 CREATE TABLE Course (
-    Course_id CHAR(5),
-	Course_name NVARCHAR(10) NOT NULL,
-	Credits INT NOT NULL,
-	Dep_code CHAR(5),
-	Teacher_id CHAR(5),
-	PRIMARY KEY(Course_id),
-	FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY(Teacher_id) REFERENCES Teacher ON UPDATE NO ACTION ON DELETE NO ACTION
+Course_id CHAR(5),
+Course_name NVARCHAR(10) NOT NULL,
+Credits INT NOT NULL,
+Dep_code CHAR(5),
+Teacher_id CHAR(5),
+PRIMARY KEY(Course_id),
+FOREIGN KEY(Dep_code) REFERENCES Department ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(Teacher_id) REFERENCES Teacher ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 
 -- 4.
 
 CREATE TABLE Grades (
-    Stu_id CHAR(5),
-	Course_id CHAR(5),
-	Grade INT NOT NULL,
-	PRIMARY KEY(Stu_id, Course_id),
-	FOREIGN KEY(Stu_id) REFERENCES Student ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY(Course_id) REFERENCES Course(Course_id) ON UPDATE NO ACTION ON DELETE NO ACTION
+Stu_id CHAR(5),
+Course_id CHAR(5),
+Grade INT NOT NULL,
+PRIMARY KEY(Stu_id, Course_id),
+FOREIGN KEY(Stu_id) REFERENCES Student ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(Course_id) REFERENCES Course(Course_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
