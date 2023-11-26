b71e69b9ef4334f10887ab078940cd1807ac4165
Jun-an <evan20021025@gmail.com>
2023-11-26 19:20:57 +0800

    Update 7-3-1_2.sql

--- CJCU_SQL_Course2023/ch07_lab/7-3-1_2.sql
+++ CJCU_SQL_Course2023/ch07_lab/7-3-1_2.sql
@@ -2,7 +2,7 @@ USE ch07_lab
 
 CREATE TABLE Product (
     P_id CHAR(5),
-	P_name NVARCHAR(10) NOT NULL,
-	Price INT,
-	PRIMARY KEY(P_id)
+    P_name NVARCHAR(10) NOT NULL,
+    Price INT,
+    PRIMARY KEY(P_id)
 )
